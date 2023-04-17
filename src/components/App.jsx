32f6c00cd662218071eaac15b98d47fea9914d66
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-04-17 19:25:30 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import React from 'react';
+import PasswordGenerator from './PasswordGenerator';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <React.Fragment>
+      <PasswordGenerator />
+    </React.Fragment>
   );
 };
---
+++ src/components/PasswordGenerator.jsx
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import { PasswordServise } from '../services/PasswordServise';
+
+function PasswordGenerator() {
+  let [state, setState] = useState({
+    generatedPassword: '',
+    passwordLength: 20,
+    symbol: false,
+    number: false,
+    lower: false,
+    upper: false,
+  });
+
+  let updateInput = event => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  let updateCheck = event => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  let submit = event => {
+    event.preventDefault();
+    let passwordObj = PasswordServise.getPasswordObj(state);
+    let thePassword = PasswordServise.generatePassword(
+      passwordObj,
+      state.passwordLength
+    );
+
+    setState({ ...state, generatedPassword: thePassword });
+  };
+
+  return (
+    <React.Fragment>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card shadow-lg">
+              <div className="card-header bg-warning p-3">
+                <p className="h4">Password Generator</p>
+              </div>
+              <div className="card-body bg-warning">
+                <form onSubmit={submit}>
+                  <div className="mb-2">
+                    <div className="input-group">
+                      <span className="input-group-text">Password</span>
+                      <input
+                        value={state.generatedPassword}
+                        onChange={updateInput}
+                        name="generatedPassword"
+                        type="text"
+                        className="form-control"
+                        placeholder="Generated Password"
+                      />
+                      <span className=" input-group-text">
+                        <i className="fa fa-clipboard"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="mb-2">
+                    <div className="input-group">
+                      <input
+                        value={state.passwordLength}
+                        onChange={updateInput}
+                        name="passwordLength"
+                        type="number"
+                        className="form-control"
+                        placeholder="Password Length"
+                        required={true}
+                      />
+                      <span className="input-group-text">Password Length</span>
+                    </div>
+                  </div>
+                  <div className="mb-2">
+                    <div className="input-group">
+                      <span className="input-group-text bg-white">
+                        <input
+                          onChange={updateCheck}
+                          name="symbol"
+                          type="checkbox"
+                          className="form-check-input"
+                        />
+                      </span>
+                      <input
+                        type="text"
+                        placeholder="Symbols"
+                        className="form-control"
+                        disabled={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-2">
+                    <div className="input-group">
+                      <span className="input-group-text bg-white">
+                        <input
+                          onChange={updateCheck}
+                          name="number"
+                          type="checkbox"
+                          className="form-check-input"
+                        />
+                      </span>
+                      <input
+                        type="text"
+                        placeholder="Numbers"
+                        className="form-control"
+                        disabled={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-2">
+                    <div className="input-group">
+                      <span className="input-group-text bg-white">
+                        <input
+                          onChange={updateCheck}
+                          name="lower"
+                          type="checkbox"
+                          className="form-check-input"
+                        />
+                      </span>
+                      <input
+                        type="text"
+                        placeholder="Lowercase Letters"
+                        className="form-control"
+                        disabled={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-2">
+                    <div className="input-group">
+                      <span className="input-group-text bg-white">
+                        <input
+                          onChange={updateCheck}
+                          name="upper"
+                          type="checkbox"
+                          className="form-check-input"
+                        />
+                      </span>
+                      <input
+                        type="text"
+                        placeholder="Uppercase Letters"
+                        className="form-control"
+                        disabled={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-2 mt-2">
+                    <input
+                      type="submit"
+                      value="Generate"
+                      className="btn btn-outline-dark"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="style-background"></div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default PasswordGenerator;
