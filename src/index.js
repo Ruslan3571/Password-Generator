9e9b8d6eefbc2f3f2695f29fc96c92be65c90904
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-04-17 17:35:50 +0300

    start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
